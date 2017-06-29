8097701f8a3dd8e8e59add24fa9ff9147ad1fb1e
Anthony Perry <atperry7@gmail.com>
2017-06-28 19:39:45 -0500

    Implement HashTag tags/{label}

--- second-assessment/src/main/java/com/cooksys/secondassessment/controller/TagController.java
+++ second-assessment/src/main/java/com/cooksys/secondassessment/controller/TagController.java
@@ -5,16 +5,15 @@ import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cooksys.secondassessment.dto.HashTagNoIdDto;
-import com.cooksys.secondassessment.entity.HashTag;
-import com.cooksys.secondassessment.entity.Tweet;
+import com.cooksys.secondassessment.dto.TweetWithIdDto;
 import com.cooksys.secondassessment.mapper.HashTagMapper;
+import com.cooksys.secondassessment.mapper.TweetMapper;
 import com.cooksys.secondassessment.service.TagService;
 
 @RestController
@@ -23,10 +22,12 @@ public class TagController {
 	
 	private TagService tService;
 	private HashTagMapper hMapper;
+	private TweetMapper tweetMapper;
 
-	public TagController(TagService tService, HashTagMapper hMapper) {
+	public TagController(TagService tService, HashTagMapper hMapper, TweetMapper tweetMapper) {
 		this.tService = tService;
 		this.hMapper = hMapper;
+		this.tweetMapper = tweetMapper;
 	}
 
 	@GetMapping("validate/tag/exists/{label}")
@@ -42,8 +43,11 @@ public class TagController {
 	}
 	
 	@GetMapping("tags/{label}")
-	public List<Tweet> getTag(@PathVariable String label, HttpServletResponse response) {
-		throw new NotYetImplementedException();
+	public List<TweetWithIdDto> getTag(@PathVariable String label, HttpServletResponse response) {
+		return tService.getTweetsWithTag(label).stream()
+				.filter(tweet -> tweet.getIsDeleted().equals(false))
+				.map(tweetMapper::tWithIdDto)
+				.collect(Collectors.toList());
 	}
 
 }
--- second-assessment/src/main/java/com/cooksys/secondassessment/service/TagService.java
+++ second-assessment/src/main/java/com/cooksys/secondassessment/service/TagService.java
@@ -1,10 +1,13 @@
 package com.cooksys.secondassessment.service;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
 import com.cooksys.secondassessment.entity.HashTag;
+import com.cooksys.secondassessment.entity.Tweet;
+import com.cooksys.secondassessment.exception.EntityNotFoundException;
 import com.cooksys.secondassessment.repository.HashTagRepository;
 
 @Service
@@ -22,6 +25,16 @@ public class TagService {
 
 	public List<HashTag> getAll() {
 		return hRepo.findAll();
+	}
+
+	public Set<Tweet> getTweetsWithTag(String label) {
+		HashTag tag = hRepo.findByLabel(label);
+		
+		if (tag != null) {
+			return tag.getUsedInTweets();
+		}
+		
+		throw new EntityNotFoundException();
 	}
 
 }
